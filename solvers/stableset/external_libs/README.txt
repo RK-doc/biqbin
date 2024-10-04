a4f54236e1f4b8eefa8a741e8012bd0b45fbd859
Roman Kuzel <roman.kuzel@rudolfovo.eu>
2024-10-04 09:11:29 +0200

    cleaing

--- solvers/stableset/external_libs/README.txt
+++
@@ -1,4 +0,0 @@
-
-https://www.mosek.com/downloads/list/8/
-
-mosektoolslinux64x86.tar.bz2 <- https://www.mosek.com/downloads/8.1.0.83/
--- solvers/stableset/external_libs/install.sh
+++
@@ -1,1 +0,0 @@
-tar xjC /tmp -f mosektoolslinux64x86.tar.bz2 && cd /tmp/mosek/8/tools/platform/linux64x86/src/fusion_cxx && make install 
